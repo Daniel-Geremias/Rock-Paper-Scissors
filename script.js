436f8f23a01789609cbb604a25708156e88bdf2f
Daniel Geremias <daniel.vg19@gmail.com>
2021-09-01 09:41:07 -0300

    Cleaned code and added style

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
-// let computerSelection;
-// let playerSelection;
-// let computerScore = 0;
-// let playerScore = 0;
-// const selection = ["rock", "paper", "scissors"];
-
-// let buttons = document.querySelectorAll("button");
-
-// buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         const selection = button.querySelector("id");
-//         playerSelection = selection;
-
-//         if (playerScore === 5 || computerScore === 5) {
-//             declareWinner();
-//         }
-//     })
-// })
-
-// function computerPlay() {
-//     return selection[Math.random() * selection.length];
-// }
-
-// function playRound(playerSelection, computerSelection) {
-//     computerSelection = computerPlay();
-// }
-
-// function declareWinner() {
-//     if (playerScore > computerScore) {
-
-//     }
-// }
-
-
 //DOM Structure
-const body = document.querySelector('body');
+const header = document.createElement('div');
+header.className = 'head';
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Rock Paper Scissors Game'
+
 const gameContainer = document.createElement('div');
 gameContainer.id = 'game';
 
+
 const rockBtn = document.createElement('button');
-rockBtn.id = 'Rock';
+rockBtn.id = 'btn';
 rockBtn.className = 'Rock'
 rockBtn.innerHTML = '<i class="far fa-hand-rock"></i>';
 
 const paperBtn = document.createElement('button');
-paperBtn.id = 'Paper';
+paperBtn.id = 'btn';
 paperBtn.className = 'Paper'
 paperBtn.innerHTML = '<i class="far fa-hand-paper"></i>';
 
 const scissorsBtn = document.createElement('button');
-scissorsBtn.id = 'Scissors';
+scissorsBtn.id = 'btn';
 scissorsBtn.className = 'Scissors'
 scissorsBtn.innerHTML = '<i class="far fa-hand-peace"></i>';
 
@@ -62,15 +34,20 @@ let score = document.createElement('p');
 score.id = 'score';
 
 const resetBtn = document.createElement('button');
+resetBtn.id = 'reset'
 resetBtn.innerText = 'Play Again';
 
-body.appendChild(gameContainer);
+document.body.appendChild(header);
+header.appendChild(h1);
+document.body.appendChild(gameContainer);
 gameContainer.appendChild(rockBtn);
 gameContainer.appendChild(paperBtn);
 gameContainer.appendChild(scissorsBtn);
-body.appendChild(scoreContainer);
+document.body.appendChild(scoreContainer);
 scoreContainer.appendChild(message);
 scoreContainer.appendChild(score);
+
+//Game Logic
 
 const selection = ['Rock', 'Paper', 'Scissors'];
 
@@ -87,6 +64,7 @@ function computerPlay() {
 function playRound(event) {
     let computerSelection = computerPlay();
     let playerSelection = event.currentTarget.className;
+
     if (playerSelection == selection[0] && computerSelection == selection[2] ||
         playerSelection == selection[1] && computerSelection == selection[0] ||
         playerSelection == selection[2] && computerSelection == selection[1]) {
@@ -104,7 +82,7 @@ function playRound(event) {
             message.innerText = 'Draw!';
             score.innerText = `You: ${playerScore} Computer: ${computerScore}`;
         }
-    console.log(playerSelection + ' VS ' + computerSelection);
+
     if(playerScore == 5) {
         return showWinner('You');
     } else if (computerScore == 5) {
@@ -114,6 +92,9 @@ function playRound(event) {
 
 function showWinner(winner) {
     message.innerText = `${winner} won!`;
+    rockBtn.setAttribute('disabled', 1);
+    paperBtn.setAttribute('disabled', 1);
+    scissorsBtn.setAttribute('disabled', 1);
     scoreContainer.appendChild(resetBtn);
     resetBtn.addEventListener('click', () => {
         location.reload();
