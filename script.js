015b90b1cbe0e7840a83dfd3badc0eff06b4c90b
Daniel Geremias <daniel.vg19@gmail.com>
2021-09-01 07:35:10 -0300

    main functions working

--- script.js
+++ script.js
@@ -38,30 +38,84 @@ const gameContainer = document.createElement('div');
 gameContainer.id = 'game';
 
 const rockBtn = document.createElement('button');
-rockBtn.id = 'rock';
-rockBtn.innerText = 'Rock';
+rockBtn.id = 'Rock';
+rockBtn.className = 'Rock'
+rockBtn.innerHTML = '<i class="far fa-hand-rock"></i>';
 
 const paperBtn = document.createElement('button');
-paperBtn.id = 'paper';
-paperBtn.innerText = 'Paper';
+paperBtn.id = 'Paper';
+paperBtn.className = 'Paper'
+paperBtn.innerHTML = '<i class="far fa-hand-paper"></i>';
 
 const scissorsBtn = document.createElement('button');
-scissorsBtn.id = 'scissors';
-scissorsBtn.innerText = 'Scissors';
+scissorsBtn.id = 'Scissors';
+scissorsBtn.className = 'Scissors'
+scissorsBtn.innerHTML = '<i class="far fa-hand-peace"></i>';
 
 const scoreContainer = document.createElement('div');
 scoreContainer.id = 'score-container';
 
-const playerScore = 0;
-const computerScore = 0;
-const score = document.createElement('p');
+const message = document.createElement('p');
+let playerScore = 0;
+let computerScore = 0;
+let score = document.createElement('p');
 score.id = 'score';
-score.innerText = `scoreText \n Player: ${playerScore} \n Computer: ${computerScore}`;
+
+const resetBtn = document.createElement('button');
+resetBtn.innerText = 'Play Again';
 
 body.appendChild(gameContainer);
 gameContainer.appendChild(rockBtn);
 gameContainer.appendChild(paperBtn);
 gameContainer.appendChild(scissorsBtn);
 body.appendChild(scoreContainer);
+scoreContainer.appendChild(message);
 scoreContainer.appendChild(score);
 
+const selection = ['Rock', 'Paper', 'Scissors'];
+
+score.innerText = `You: ${playerScore} Computer: ${computerScore}`;
+
+const button = [...document.querySelectorAll('button')].forEach(function(item) {
+    item.addEventListener('click', (playerScore < 5 && computerScore < 5) ? playRound : showWinner);
+});
+
+function computerPlay() {
+    return selection[Math.floor(Math.random() * selection.length)];
+}
+
+function playRound(event) {
+    let computerSelection = computerPlay();
+    let playerSelection = event.currentTarget.className;
+    if (playerSelection == selection[0] && computerSelection == selection[2] ||
+        playerSelection == selection[1] && computerSelection == selection[0] ||
+        playerSelection == selection[2] && computerSelection == selection[1]) {
+            playerScore++;
+            message.innerText = `You scored! ${playerSelection} beats ${computerSelection}`;
+            score.innerText = `You: ${playerScore} Computer: ${computerScore}`;
+        } else
+    if (playerSelection == selection[0] && computerSelection == selection[1] ||
+        playerSelection == selection[1] && computerSelection == selection[2] ||
+        playerSelection == selection[2] && computerSelection == selection[0]) {
+            computerScore++;
+            message.innerText = `Computer scored! ${computerSelection} beats ${playerSelection}`;
+            score.innerText = `You: ${playerScore} Computer: ${computerScore}`;
+        } else {
+            message.innerText = 'Draw!';
+            score.innerText = `You: ${playerScore} Computer: ${computerScore}`;
+        }
+    console.log(playerSelection + ' VS ' + computerSelection);
+    if(playerScore == 5) {
+        return showWinner('You');
+    } else if (computerScore == 5) {
+        return showWinner('Computer');
+    }
+}
+
+function showWinner(winner) {
+    message.innerText = `${winner} won!`;
+    scoreContainer.appendChild(resetBtn);
+    resetBtn.addEventListener('click', () => {
+        location.reload();
+    });
+}
